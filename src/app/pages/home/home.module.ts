a136c2737e75d1c6cc27a1f08451b216ac904065
matias narvaja <matyas_93_8@hotmail.com>
2025-06-21 11:54:28 -0300

    ajustes responsive home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,12 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     RouterModule,
